8e9ef9f51b4384f1db5104692361e3d709359681
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-07-16 16:17:01 -0400

    finished base functionality

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Mark_Me_Down
+
+    # Mark Me Down
+
+    [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+    ## Description
+      This 
+
+    ## Installation
+      ja
+
+    ## Usage
+      ms
+
+    ## Contributers
+      s
+
+    ## Testing Instructions
+      kmec
+
+    ##Contact Me
+      ce ce
--- index.js
+++ index.js
@@ -1,21 +1,117 @@
-let info = ['Title,', 'Description', 'Installation Process', 'Usage', 'Contributers', 'Testing Process', 'Your Contact Information'];
+const { default: inquirer } = require('inquirer');
 
-function acceptInfo(index) {
-  if (index == 5) {
-    process.exit();
+const fs = require('fs').promises;
+
+const fields = [
+  {
+    name: 'title',
+    message: 'Please enter a Title'
+  },
+
+  {
+    name: 'description',
+    message: 'Please enter a Description'
+  },
+
+  {
+    name: 'install',
+    message: 'Please enter the install process'
+  },
+
+  {
+    name: 'usage',
+    message: 'Please enter the usage information'
+  },
+
+  {
+    name: 'conts',
+    message: 'Please enter any Contributers'
+  },
+
+  {
+    name: 'licenses',
+    message: 'Which license do you own?',
+    type: 'list',
+    choices: [
+      'The MIT License',
+      'Apache License 2.0',
+      'GNU GPL v3',
+      'Mozilla Public License 2.0',
+      'BSD 3-Clause License'
+    ]
+  },
+
+  {
+    name: 'test',
+    message: 'Please enter the testing directions'
+  },
+
+  {
+    name: 'git',
+    message: 'Please enter your GitHub username'
+  },
+
+  {
+    name: 'mail',
+    message: 'Please enter your email address'
   }
-  console.log(`
-Please Enter the ${info[index]}
-`);
-  acceptInfo(++index);
+]
+
+function acceptInfo() {
+  inquirer.prompt(fields)
+    .then(fieldObj => writeMD(fieldObj))
+};
+
+function fetchLicense(license) {
+  switch (license) {
+    case 'The MIT License':
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    case 'Apache License 2.0':
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    case 'GNU GPL v3':
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    case 'Mozilla Public License 2.0':
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    case 'BSD 3-Clause License':
+      return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+  }
 }
+
+function writeMD(vals) {
+  const license = fetchLicense(vals.licenses);
+  const md = `
+    # ${vals.title}
+
+    ${license}
+
+    ## Description
+      ${vals.description}
+
+    ## Installation
+      ${vals.install}
+
+    ## Usage
+      ${vals.usage}
+
+    ## Contributers
+      ${vals.conts}
+
+    ## Testing Instructions
+      ${vals.test}
+
+    ##Contact Me
+      ${vals.git} ${vals.mail}
+`;
+  return fs.writeFile('./README.md', md);
+}
+
 function init() {
   console.log(`
 ---------------------------
   Welcome To Mark Me Down!
 ---------------------------
 `);
-  acceptInfo(0);
-}
+  acceptInfo()
+};
 
 init();
